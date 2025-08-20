694618c294c6b84e3e7ce26b7d685e6155b2819f
David White <whited9339@gmail.com>
2025-08-20 17:17:36 -0600

    Update index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,1 @@
-export { Config, ProjectCache, ScriptCache } from "./cache";
-export * as Notion from "./notion";
-export * as Formatting from "./format";
-export * as Storyboard from "./storyboard";
-export * as Sync from "./sync";
-export * as Project from "./project";
-export * as Script from "./script";
+export () => console.log("Core package not yet implemented");
